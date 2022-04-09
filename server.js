b241ef7ccc62d713fc5c2718e872efeec0fc35f1
Nina2023 <nsehgal2019@gmail.com>
2022-04-09 17:48:05 -0400

    changed server again

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-// requirements 
+// require things
 const express = require('express')
-const minimist = require("minimist")
-const args = require('minimist')(process.argv.slice(2))
-const coin_functions = require('./modules/coin')
 const app = express()
 
+const args = require('minimist')(process.argv.slice(2))
 args["port"]
 const port = args.port || 5000
 
+const coin = require('./modules/coin')
 
-//start server
-
+// start app server
 const server = app.listen(port, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%', port))
-});
+    console.log('App listening on port %PORT%'.replace("%PORT%", port))
+})
 
 // define endpoints
 app.get('/app/', (req, res) => {
@@ -23,30 +21,26 @@ app.get('/app/', (req, res) => {
     res.end(res.statusCode + ' ' + res.statusMessage)
 })
 
-
-app.get('/app/flip/call/tails', (req, res) => {
-    const r = coin_functions.flipACoin("tails")
-    res.status(200).json({call: r.call, flip: r.flip, result: r.result})
-})
-
-app.get('/app/flip/call/heads', (req, res) => {
-    const result = coin_functions.flipACoin("heads")
-    res.status(200).json({call: result.call, flip: result.flip, result: result.result})
-})
-
 app.get('/app/flip/', (req, res) => {
-    res.status(200).json({flip: coin_functions.coinFlip()})
+    res.status(200).json({flip: coin.coinFlip()})
 })
 
 app.get('/app/flips/:number', (req, res) => {
-    const flips = coin_functions.coinFlips(req.params.number)
-    res.status(200).json({raw: flips, summary: coin_functionscountFlips(flips)})
+    const flips = coin.coinFlips(req.params.number)
+    res.status(200).json({raw: flips, summary: coin.countFlips(flips)})
 })
 
+app.get('/app/flip/call/heads', (req, res) => {
+    const result = coin.flipACoin("heads")
+    res.status(200).json({call: result.call, flip: result.flip, result: result.result})
+})
 
+app.get('/app/flip/call/tails', (req, res) => {
+    const result = coin.flipACoin("tails")
+    res.status(200).json({call: result.call, flip: result.flip, result: result.result})
+})
 
-
-// error message
+// default response for any other request
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
     res.type('text/plain')
